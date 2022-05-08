cd6735fc1d7ed9aa2015dfae3a1a837c266b720f
mklzc <mklzc@example.com>
2022-05-08 14:31:18 +0800

    fast read

--- xor/xor.cpp
+++ xor/xor.cpp
@@ -1,5 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+namespace FastIO {
+    char gc() {
+        static char buf[1 << 24], *is = buf, *it = buf;
+        if (is == it) is = buf, it = is + fread(buf, 1, 1 << 24, stdin);
+        return is == it ? EOF : *is ++;
+    }
+    char out[1 << 24];
+    int len;
+    void flush() { fwrite(out, 1, len, stdout), len = 0; }
+    void pc(char x) {
+        if (len == 1 << 24) flush();
+        out[len ++] = x;
+    }
+    struct Flusher {
+        ~Flusher() { flush(); }
+    } Fls;
+    // #define gc() getchar()
+    // #define pc(X) putchar(X)
+    template <class T>
+    inline void read(T &x)
+    {
+        char c, flag = 0;
+        while ((c = gc()) < '0' || c > '9') flag |= c == '-';
+        x = c & 15;
+        while ((c = gc()) >= '0' && c <= '9')
+            x = (x << 3) + (x << 1) + (c & 15);
+        if (flag) x = ~x + 1;
+    }
+    template <class T, class ...T1>
+    inline void read(T &x, T1 &...x1) { read(x), read(x1...); }
+    template <class T>
+    inline void _put(T x)
+    {
+        if (x > 9) _put(x / 10);
+        pc((x % 10) | 48);
+    }
+    template <class T>
+    inline void write(T x) {
+        if (x < 0) pc('-'), x = ~x + 1;
+        _put(x);
+    }
+    template <> inline void write(char x) { pc(x); }
+    template <class T, class ...T1>
+    inline void write(T x, T1 ...x1) { write(x), write(x1...); }
+}
+using FastIO::read;
+using FastIO::write;
+
+
 const int N = 1e5 + 5;
 int s[N], fa[N], v[N], ans, flag;
 struct Node
@@ -41,11 +91,11 @@ int main()
     freopen("xor4.in", "r", stdin);
     freopen("xor.out", "w", stdout);
     int n, m, l, r, x;
-    scanf("%d%d", &n, &m);
+    read(n, m);
     for (int i = 1; i <= n; i++) fa[i] = i;
     while (m--)
     {
-        scanf("%d%d%d", &l, &r, &x);
+        read(l, r, x);
         if (get(l - 1) == get(r))
         {
             memset(v, 0, sizeof(v));
@@ -55,9 +105,9 @@ int main()
             for (auto a : G[l - 1])
                 if (a.to == r) ans = a.cost;
             if (ans != x)
-                {puts("No");continue;}
+                {write('N', 'o', '\n');continue;}
         }
-        puts("Yes");
+        write('Y', 'e', 's', '\n');
         Node a = {to: l - 1, cost: x}, b = {to: r, cost: x};
         G[l - 1].push_back(b), G[r].push_back(a);
         fa[get(l - 1)] = get(r);
@@ -69,7 +119,7 @@ int main()
         s[i] ^= s[0];
     s[0] = 0;
     for (int i = 1; i <= n; i++)
-        printf("%d ", s[i] ^ s[i - 1]);
-    printf("\n");
+        write(s[i] ^ s[i - 1], ' ');
+    write('\n');
     return 0;
 }
