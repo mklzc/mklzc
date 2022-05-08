080c58f67bd82d0bad3ce7892303188db6120995
mklzc <mklzc@example.com>
2022-05-08 10:30:05 +0800

    xor finish

--- xor/xor.cpp
+++ xor/xor.cpp
@@ -1,94 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e3 + 5;
-int s[N];
-int fa[N], v[N];
+const int N = 1e5 + 5;
+int s[N], fa[N], v[N], ans, flag;;
 struct Node
 {
     int to;
     int cost;
 };
 vector<Node> G[N];
-void add(int x, int y, int z)
-{
-    Node a = {to: y, cost: z};
-    G[x].push_back(a);
-}
 int get(int x)
 {
     if (fa[x] != x) return fa[x] = get(fa[x]);
     return fa[x];
 }
-
-int ans, flag;
-void find_circle(int x, int ANS, int END)
+void get_path(int x, int ANS, int END)
 {
     if (flag) return;
     if (x == END) return void(flag = 1), void(ans ^= ANS);
-    for (int i = 0; i < G[x].size(); i++)
+    for (auto a : G[x])
     {
-        int y = G[x][i].to, z = G[x][i].cost;
+        int y = a.to, z = a.cost;
         if (v[y]) continue;
         v[y] = 1;
-        find_circle(y, ANS ^ z, END);
+        get_path(y, ANS ^ z, END);
     }
 }
 int main()
 {
-    freopen("in", "r", stdin);
-    int n, m;
+    freopen("xor.in", "r", stdin);
+    freopen("xor.out", "w", stdout);
+    int n, m, l, r, x;;
     scanf("%d%d", &n, &m);
     for (int i = 1; i <= n; i++) fa[i] = i;
     while (m--)
     {
-        int l, r, x;
         scanf("%d%d%d", &l, &r, &x);
         if (get(l - 1) == get(r))
         {
             memset(v, 0, sizeof(v));
             ans = 0, flag = 0;
-            find_circle(l - 1, 0, r), flag = 0;
-            find_circle(r, 0, l - 1);
+            get_path(l - 1, 0, r), flag = 0;
+            get_path(r, 0, l - 1);
             if (ans != x)
                 {puts("No");continue;}
         }
         puts("Yes");
         Node a = {to: l - 1, cost: x}, b = {to: r, cost: x};
-        G[l - 1].push_back(b);
-        G[r].push_back(a);
+        G[l - 1].push_back(b), G[r].push_back(a);
         fa[get(l - 1)] = get(r);
     }
     s[0] = 0;
     for (int i = 0; i <= n; i++)
-        for (int j = 0; j < G[i].size(); j++)
-        {
-            int y = G[i][j].to, z = G[i][j].cost;
-            if (y <= i) continue;
-            s[y] = s[i] ^ z;
-        }
+        for (auto a : G[i])
+            if (a.to > i) s[a.to] = s[i] ^ a.cost;
     for (int i = 1; i <= n; i++)
         printf("%d ", s[i] ^ s[i - 1]);
     printf("\n");
     return 0;
 }
-/*
-Sample2:
-
-5 6
-1 4 4
-2 3 3
-1 4 2
-2 5 3 
-1 1 1
-4 5 1
-
-stdout:
-Yes
-Yes
-No
-Yes
-Yes
-No
-1 1 2 6 6
-
-*/
